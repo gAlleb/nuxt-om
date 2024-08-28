ddd7f07e996e7524cff7c4b727ce89de7c4ef144
Stefan <vikeevsv@gmail.com>
2024-08-28 12:48:07 +0300

    remove JIT from tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
+  // mode: 'jit',
   content: ['./components/**/*.{js,vue,ts}', './layouts/**/*.vue', './pages/**/*.vue', './plugins/**/*.{js,ts}', './content/**/*.{md,js,ts}', './nuxt.config.{js,ts}', './app.vue'],
   theme: {
     extend: {
