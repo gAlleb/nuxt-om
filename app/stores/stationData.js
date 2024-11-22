30cc57acc02a1c7c02047b9b7963be56fcf83709
Stefan <vikeevsv@gmail.com>
2024-11-22 13:25:22 +0300

    .

--- app/stores/stationData.js
+++ app/stores/stationData.js
@@ -225,7 +225,7 @@ export const useAzuracastData = defineStore({
     async fetchCoverArtForSongHistory(songHistory, station) {
       const historyToFetch = songHistory.slice(0, 5); 
       historyToFetch.forEach((song, index) => {
-        this.fetchCoverArt(song.song.artis, song.song.title, station)
+        this.fetchCoverArt(song.song.artist, song.song.title, station)
           .then(coverArtUrl => {
             if (!this.songHistoryCoverArt[station]) {
               this.songHistoryCoverArt[station] = {};
