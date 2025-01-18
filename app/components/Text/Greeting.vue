b6c482b046ede83dfbe6338d47705eae9f818fd0
Stefan <vikeevsv@gmail.com>
2025-01-18 08:22:50 +0300

    .

--- app/components/Text/Greeting.vue
+++ app/components/Text/Greeting.vue
@@ -2,12 +2,12 @@
  <div class="mt-10 mx-5 sm:mx-5 md:mx-12 lg:mx-12 rounded-3xl shadow-lg bg-sxvx-light dark:bg-sxvx-dark h-100">
   <div class="p-10 text-center">
     <h4 class="mt-0 bold text-2xl">
-    Welcome to <span class="text-red-500">omFM</span>
+    <span class="text-red-500">omFM</span> Radio
     </h4>
     <h4 class="mt-0 bold text-md">
     Not-trivial collection of streams 
     </h4>
-    <p class="italic text-md text-muddy-waters-500">
+    <p class="italic text-md text-muddy-waters-700 dark:text-muddy-waters-500">
     The musical art often speaks in sounds more penetrating than the words of poetry, and takes hold of the most hidden crevices of the heart… Song elevates our being and leads us to the good and the true...<br/>
    </p>
    <p class="text-lg">
