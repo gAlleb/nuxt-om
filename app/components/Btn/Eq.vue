cc9b56a96650a60cbfc2af64448d908ce9bc98e0
Stefan <vikeevsv@gmail.com>
2025-01-28 18:47:21 +0300

    Make EQ background transitions smooth. Let EQ link button act like Page Switcher

--- app/components/Btn/Eq.vue
+++ app/components/Btn/Eq.vue
@@ -1,9 +1,11 @@
 <template>
 <UTooltip text="Equalizer" :popper="{ offsetDistance: 5 }"  class="">
-<NuxtLink :to="localePath('/eq')" class="text-zinc-600 dark:text-zinc-100">
+<NuxtLink :to="targetRoute" class="text-zinc-600 dark:text-zinc-100">
 <button
-class="flex ml-1 sm:ml-4 hover:cursor-pointer rounded-xl bg-sxvx-light-bg dark:bg-sxvx-dark-bg text-sm   
+class="flex ml-1 sm:ml-4 hover:cursor-pointer rounded-xl text-sm   
 active:ring-2 active:ring-gray-300 focus:ring-offset focus:ring-offset-gray-800 p-2" 
+:class="{'bg-sxvx-light-bg dark:bg-sxvx-dark-bg': !isEqPage,
+         'bg-green-600': isEqPage}"
 >
 <span class="sr-only">Equalizer</span>
 <Icon name="bx:equalizer" class="h-6 w-6 text-zinc-600 dark:text-zinc-100" aria-hidden="true" />
@@ -13,5 +15,30 @@ active:ring-2 active:ring-gray-300 focus:ring-offset focus:ring-offset-gray-800 
 </template>
 
 <script setup>
-const localePath = useLocalePath()
+import { useRoute } from '#imports'
+import { ref, computed } from 'vue'
+const localePath = useLocalePath();
+const route = useRoute()
+const previousRoute = ref(null) 
+const isEqPage = ref(false)
+
+onMounted(()=>{
+if (route.path === '/eq') {
+  previousRoute.value = '/'
+  isEqPage.value = true;
+} else {
+  previousRoute.value = route.path
+  isEqPage.value = false;
+}
+})
+const targetRoute = computed(() => {
+  if (route.path === '/eq') {
+    isEqPage.value = true;
+    return previousRoute.value || '/'
+  } else {
+    previousRoute.value = route.path;
+    isEqPage.value = false;
+    return localePath('/eq')
+  }
+})
 </script>
--- app/pages/eq.vue
+++ app/pages/eq.vue
@@ -4,13 +4,21 @@
  
 
  <div  class="absolute h-screen w-full" style="background: rgba(0, 0, 0, .7); z-index: 1;"/>
- <div v-if="currentStream === 'stream' && np_omfm.coverArtUrls['station:radio'] && useInitPlayerStore.isPlaying" class="blur-sm h-screen absolute w-full transition-all duration-500" :style="{ background: `url(${np_omfm.coverArtUrls['station:radio']})`, backgroundPosition: 'center', backgroundSize: 'cover' }"/>
- <div v-if="currentStream === 'rock' && np_ac.coverArtUrls['station:radio'] && useInitPlayerStore.isPlaying" class="blur-sm h-screen absolute w-full transition-all duration-500" :style="{ background: `url(${np_ac.coverArtUrls['station:radio']})`,   backgroundPosition: 'center', backgroundSize: 'cover' }"/>
- <div v-if="currentStream === 'coma' && comaData && useInitPlayerStore.isPlaying" class="blur-sm h-screen absolute w-full transition-all duration-500" :style="{ background: `url(${comaData.np.now_playing.song.art})`, backgroundPosition: 'center', backgroundSize: 'cover' }"/>
- <div v-if="currentStream === 'terra' && terraData && useInitPlayerStore.isPlaying" class="blur-sm h-screen absolute w-full transition-all duration-500" :style="{ background: `url(${terraData.np.now_playing.song.art})`, backgroundPosition: 'center', backgroundSize: 'cover' }"/>
- <div v-if="currentStream === 'core' && coreData && useInitPlayerStore.isPlaying" class="blur-sm h-screen absolute w-full transition-all duration-500" :style="{ background: `url(${coreData.np.now_playing.song.art})`, backgroundPosition: 'center', backgroundSize: 'cover' }"/>
-
-
+ <!-- <div v-if="currentStream === 'stream' && np_omfm.coverArtUrls['station:radio'] && useInitPlayerStore.isPlaying" data-aos="fade-in" data-aos-duration="3000" class="blur-sm h-screen absolute w-full transition-all duration-3000" :style="{ background: `url(${np_omfm.coverArtUrls['station:radio']})`, backgroundPosition: 'center', backgroundSize: 'cover' }"/>
+ <div v-if="currentStream === 'rock' && np_ac.coverArtUrls['station:radio'] && useInitPlayerStore.isPlaying"  data-aos="fade-in" data-aos-duration="3000" class="blur-sm h-screen absolute w-full transition-all duration-3000" :style="{ background: `url(${np_ac.coverArtUrls['station:radio']})`,   backgroundPosition: 'center', backgroundSize: 'cover' }"/>
+ <div v-if="currentStream === 'coma' && comaData && useInitPlayerStore.isPlaying"  data-aos="fade-in" data-aos-duration="3000" class="blur-sm h-screen absolute w-full transition-all duration-3000" :style="{ backgroundImage: `url(${comaData.np.now_playing.song.art})`, backgroundPosition: 'center', backgroundSize: 'cover' }"/>
+ <div v-if="currentStream === 'terra' && terraData && useInitPlayerStore.isPlaying"  data-aos="fade-in" data-aos-duration="3000" class="blur-sm h-screen absolute w-full transition-all duration-3000" :style="{ backgroundImage: `url(${terraData.np.now_playing.song.art})`, backgroundPosition: 'center', backgroundSize: 'cover' }"/>
+ <div v-if="currentStream === 'core' && coreData && useInitPlayerStore.isPlaying"  data-aos="fade-in" data-aos-duration="30000" class="blur-sm h-screen absolute w-full transition-all duration-3000" :style="{ backgroundImage: `url(${coreData.np.now_playing.song.art})`, backgroundPosition: 'center', backgroundSize: 'cover' }"/>
+ -->
+ <div 
+      class="blur-sm h-screen absolute w-full" 
+      style="transition: background-image 2s ease-in"
+      :style="{ 
+        backgroundImage: backgroundImage,
+        backgroundPosition: 'center', 
+        backgroundSize: 'cover' 
+      }"
+    />
  
   <div class="relative overflow-hidden h-48 sm:h-40" style="z-index: 2;"> 
     <Visualizer3Waves :color-scheme="{ color1: 'grey', color2: 'red', color3: 'orange' }" :custom-dark-scheme="{ color1: 'wheat', color2: 'yellow', color3: 'orange' }"/>
@@ -38,12 +46,22 @@ import { useAzuracastData } from '../stores/stationData';
 const np_ac = useAzuracastData();
 import { useOmfmData } from '../stores/stationData_omfm';
 const np_omfm = useOmfmData();
-const omfmData = computed(() => np_omfm.stations['station:radio']);
-const radioData = computed(() => np_ac.stations['station:radio']);
-const comaData = computed(() => np_ac.stations['station:coma']);
-const terraData = computed(() => np_ac.stations['station:terra']);
-const coreData = computed(() => np_ac.stations['station:core']);
-
+const backgroundImage = computed(() => {
+  if (useInitPlayerStore.isPlaying) {
+    if (currentStream.value === 'stream' && np_omfm.coverArtUrls['station:radio']) {
+      return `url(${np_omfm.coverArtUrls['station:radio']})`;
+    } else if (currentStream.value === 'rock' && np_ac.coverArtUrls['station:radio']) {
+      return `url(${np_ac.coverArtUrls['station:radio']})`;
+    } else if (currentStream.value === 'coma' && np_ac.stations['station:coma']?.np?.now_playing?.song?.art) {
+      return `url(${np_ac.stations['station:coma'].np.now_playing.song.art})`;
+    } else if (currentStream.value === 'terra' && np_ac.stations['station:terra']?.np?.now_playing?.song?.art) {
+      return `url(${np_ac.stations['station:terra'].np.now_playing.song.art})`;
+    } else if (currentStream.value === 'core' && np_ac.stations['station:core']?.np?.now_playing?.song?.art) {
+      return `url(${np_ac.stations['station:core'].np.now_playing.song.art})`;
+    }
+  }
+  return 'url(/static/img/defaultCoverart.jpg)';
+});
 
 </script>
 <style scoped>
