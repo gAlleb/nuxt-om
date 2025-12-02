1658a6534cadb1b3fdf3bf827be0bbddf856fe98
Stefan <144447266+gAlleb@users.noreply.github.com>
2025-12-02 18:01:41 +0300

    Update nuxt.config.ts   ssr: false,

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
+  ssr: false,
   // nitro: { 
   //   prerender: {
   //     routes: ["/blog", "/blog/articles","/","/es/blog"],
